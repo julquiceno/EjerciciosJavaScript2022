279269622ab958e72abc196d53f866bdc93206e8
julquiceno <julquiceno@misena.edu.co>
2022-11-08 22:15:15 -0500

    Primer ejercicio JS

---
+++ js/main.js
@@ -0,0 +1,15 @@
+// alert("Hola mundo esoy aprendiendo JavaScript")
+
+// Este es un comentario de linea
+
+// alt + shift + a = metodo abreviado para comentario de bloque
+
+// Ctrl + } = metodo abreviado para el comentario de linea
+
+console.log("Hola este es un mensaje por consola")
+console.log(1234567)
+console.log("Julián")
+console.log(false)
+console.log(true)
+
+document.write("Hola estoy escribiendo en la pagina web")
